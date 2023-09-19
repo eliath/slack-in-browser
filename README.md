38f329dcb74adbe93b8e4332f228c8fdb86a2d1c
Elias Martinez Cohen <emc@eliath.biz>
2023-09-19 11:27:56 -0600

    Copy source code from chrome extension
    From https://chrome.google.com/webstore/detail/open-slack-in-browser-not/jkgehijlkoolgcjifalbiicaomkngakb
    Used https://robwu.nl/crxviewer/
    Verified with FF temp extension
    
    Consider this v0.1

---
+++ README.md
@@ -0,0 +1,8 @@
+# slack-in-browser
+
+I wanted
+[this](https://chrome.google.com/webstore/detail/open-slack-in-browser-not/jkgehijlkoolgcjifalbiicaomkngakb)
+extension to work in firefox. So I cloned it. Thank you, yume :\)
+
+
+Added a simple exponential backoff instead of constant 1sec wait time. Otherwise it is the same.
---
+++ content.js
@@ -0,0 +1,31 @@
+const minBackoff = 200;   // 200ms
+const maxBackoff = 10000; // 10s
+
+const backoff = async (depth = 0) => {
+    let waitTime = 2 ** depth;
+    if (waitTime < minBackoff) { waitTime = minBackoff }
+    if (waitTime > maxBackoff) { waitTime = maxBackoff }
+    await new Promise(resolve => setTimeout(resolve, waitTime));
+};
+
+
+const init = async () => {
+    let link = "";
+    for (let i = 0; i < 16; i++) {
+        try {
+            let a = Array.from(document.querySelectorAll("a"))
+                .find(a => a.href.includes("/messages/"));
+            link = a.href;
+        } catch (error) {
+            console.log(`[slack-in-browser] link not found (attempt ${i})`);
+            await backoff(i);
+        }
+    }
+    if (link === "") {
+        alert("ask extension developer code@eliath.biz to fix this issue");
+    } else {
+        window.location = link;
+    }
+};
+
+init();
